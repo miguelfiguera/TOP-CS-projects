2d4f3f64c96e6b814c55461b07238e1a674dece3
Miguel Figuera <miguelqui725@gmail.com>
2022-06-07 16:50:09 -0400

    fibs in one line or two

--- recursive fibonacci.rb
+++ recursive fibonacci.rb
@@ -2,13 +2,9 @@ def fibs(n,fib=[0,1])
 until n == fib.length do
     fib.push(fib[-1]+fib[-2])
 end
+fib
 end
 
-def fibs_rec(n ,fib_arr=[0,1])
-    if n == fib_arr.length
-        fib_arr
-    else
-    fib_arr.push(fib_arr[-1]+fib_arr[-2])
-    fibs_rec(n,fib_arr)
-    end   
+def fibs_rec(n ,fib=[0,1])
+    n==fib.length ? fib : fib.push(fib[-1] + fib[-2]) && fibs_rec(n,fib)
 end
