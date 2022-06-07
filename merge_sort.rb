3a154180daeb3b18d559e693475e3353aad8af6c
Miguel Figuera <miguelqui725@gmail.com>
2022-06-07 17:23:41 -0400

    working on merge_sort

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,1 +1,17 @@
 not_sorted = [1,5,2,6,7,8,4,3]
+
+def merge_sort(array)
+    n = array.length
+    return array if n < 2
+    a = merge_sort(array[0...(n/2)])
+    b = merge_sort(array[(n/2..-1)])
+    p merge(a,b)
+end
+
+def merge(left, right)
+    a = []
+    until left.empty? || right.empty?
+        a.push(left[0] <= right[0] ? left.shift : right.shift)
+    end
+    a + left + right
+end
